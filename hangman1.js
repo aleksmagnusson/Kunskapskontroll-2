dab72d256a38b8b721b8ed3c79c7d8aa1383f06d
AleksaNatalia <92426232+AleksaNatalia@users.noreply.github.com>
2021-11-27 23:45:50 +0100

    Kunskapskontroll_2_V_2
    Kunskapsprov, nästan slutlig fil. Lämnas in imorgon.

--- hangman1.js
+++ hangman1.js
@@ -1,31 +1,30 @@
-// Möjliga ord som dras slumpmässigt. Sätts in i en const.
-
 const words = [
-    "Afterwork", "Javascript",
-    "Stockholm", "Studenter",
-    "Programmering",
+  "Afterwork", "Javascript",
+  "Stockholm", "Studenter",
+  "Programmering",
 ];
 
 // Slumpfunktion som ger olika ord för varje omgång.
 let randomWord = words[Math.floor(Math.random() * words.length)];
 // Stora och små bokstäver.
-let randomWordUpc = randomWord.toUpperCase();
+let randomWordUpc = randomWord.toLowerCase();
+console.log(randomWord);
 
 // Livlinor --> "Lives: <number>"
-Let lives = 5;
+let lives = 5;
 
 let rightInput = randomWord.length;
 // Dolda versaler och gemener.
 let hiddenCharacters = randomWord.split("");
 
-let playGuess = [];
+const playGuess = [];
 
 // Funktion for(let...).
 
 function startPlay() {
-    for (let i = 0; i < randomWord.length; i++) {
-        hiddenCharacters[i] = "_";
-    }
+  for (let i = 0; i < randomWord.length; i++) {
+    hiddenCharacters[i] = "_";
+  }
 }
 
 startPlay();
@@ -33,36 +32,41 @@ startPlay();
 console.log(randomWord);
 
 do {
-    let guessInput = prompt(`${hiddenCharacters.join("")} \n Lives left: ${lives}\n Guesses made: ${playGuess}`);
+  let guessInput = prompt(
+    `${hiddenCharacters.join(
+      " "
+    )} \n Lives left: ${lives}\n Guesses made: ${playGuess}`
+  );
 
-    if (guessInput === null) {
-        alert("You have canceled the game.");
-        break;
-    } 
-}
+  if (guessInput === null) {
+    alert("You have canceled the game.");
+    break;
+  }
 
-guessInput = guessInput.toUpperCase();
+  while ((guessInput = guessInput.toUpperCase()));
 
-(!guessInput.search(/[^a-zA-Z]+/) || guessInput.length !== 1) {
+  !guessInput.search(/[^a-zA-Z]+/) || guessInput.length !== 1;
+  {
     alert("Enter only letters between A to Z.");
     continue;
-} else if (randomWordUpc.includes(guessInput)) {
+  }
+  if (randomWordUpc.includes(guessInput)) {
     for (let i = 0; i < randomWord.length; i++) {
-        if (guessInput === randomWordUpc[i]) {
-            hiddenCharacters[i] = randomWord[i];
-            rightInput--;
-        }
+      if (guessInput === randomWordUpc[i]) {
+        hiddenCharacters[i] = randomWord[i];
+        rightInput--;
+      }
     }
-} else {
-lives--;
-}
+  } else {
+    lives--;
+  }
 
-if (!randomWordUpc.includes(guessInput)) {
+  if (!randomWordUpc.includes(guessInput)) {
     playGuess.push(guessInput);
-} if (lives == 0) {
+  }
+  if (lives == 0) {
     alert(`You have lost the game. The correct word was ${randomWord}`);
-    else if (hiddenCharacters.join("") === randomWord) {
-        alert("You have won, congratulations!");
-    }
-}
-while (lives > 0 && rightInput > 0);
+  } else if (hiddenCharacters.join("") === randomWord) {
+    alert("You have won, congratulations!");
+  }
+} while (lives > 0 && rightInput > 0);
