17231579828bf09b9e0967e6b350fd4269861e4a
AleksaNatalia <92426232+AleksaNatalia@users.noreply.github.com>
2021-11-27 16:16:10 +0100

    HangManKK2
    Made a separeted map for this game. To upload to the assignment.

---
+++ hangman1.js
@@ -0,0 +1,45 @@
+// Möjliga ord som dras slumpmässigt. Sätts in i en const.
+
+const words = [
+    "Afterwork", "Javascript",
+    "Stockholm", "Studenter",
+    "Programmering",
+];
+
+// Slumpfunktion som ger olika ord för varje omgång.
+let randomWord = words[Math.floor(Math.random() * words.length)];
+// Stora och små bokstäver.
+let randomWordUpc = randomWord.toUpperCase();
+
+// Livlinor -->
+Let lives = 5;
+
+let rightInput = randomWord.length;
+// Dolda versaler och gemener.
+let hiddenCharacters = randomWord.split("");
+
+let playGuess = [];
+
+// Funktion for(let...).
+
+function startPlay() {
+    for (let i = 0; i < randomWord.length; i++) {
+        hiddenCharacters[i] = "_";
+    }
+}
+
+startPlay();
+
+console.log(randomWord);
+
+do {
+    let guessInput = prompt(`${hiddenCharacters.join("")} \n Lives left: ${lives}\n Guesses made: ${playGuess}`);
+
+    if (guessInput === null) {
+        alert("You have canceled the game.");
+        break;
+    } 
+}
+
+guessInput = guessInput.toUpperCase();
+
